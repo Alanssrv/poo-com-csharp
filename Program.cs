a6e55166ecbae3a0e0d6b6adbce75551a110fd5e
Alan Sousa da Silva <alanssrv@gmail.com>
2022-02-08 15:02:27 -0300

    full project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using RPG.src.Entities;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard jennica = new Wizard("Jennica", 99, "White Wizard");
+
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(jennica.Attack(7));
+            Console.WriteLine(jennica.Attack(1));
+        }
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# Aprendizado de POO com C#
+
+* ### Projeto da GFT Start #4 .NET
+* ### Disponível na plataforma [DIO](https://web.dio.me/home)
